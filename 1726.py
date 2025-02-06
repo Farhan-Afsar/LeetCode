036a593397b6c8bcbca40b4d9187a57355acb5e6
Farhan Afsar <mdfarhanafsar@gmail.com>
2025-02-06 15:16:38 +0600

    Solution

---
+++ 1726.py
@@ -0,0 +1,16 @@
+class Solution:
+    def tupleSameProduct(self, nums: List[int]) -> int:
+
+        d = {}
+        res = 0
+
+        for i in range(len(nums)):
+            for j in range(i+1,len(nums)):
+                product = nums[i] * nums[j]
+
+                if product in d:
+                    res += d[product] * 8
+                    d[product] += 1
+                else:
+                    d[product] = 1
+        return res
